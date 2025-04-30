1af9f0cb82840195b854299187ebe3d6238b91d0
Luca <lucm@itu.dk>
2025-04-30 10:11:25 +0200

    Added getImages to img_util.py :)

--- main_baseline.py
+++ main_baseline.py
@@ -10,47 +10,40 @@ from util.feature_B import compactness
 from util.feature_C import rgb_var
 from util.feature_C import slic_segmentation
 from util.classifier_kNN import knn_algorithm_smote
+from util.img_util import getImages
+
 
 def extract_features(folder_dir):
-  feature_list = []
-  for filename in os.listdir(folder_dir):
-      if filename.endswith(".png"):
-          img_id = filename 
-          file_path = os.path.join(folder_dir, filename)
-          img = cv2.imread(file_path)
-          
-          #resize if needed
-          max_size = 256
-          h, w = img.shape[:2]
-          scale = min(max_size / w, max_size / h)
-          if scale < 1:
-              new_size = (int(w * scale), int(h * scale))
-              img = cv2.resize(img, new_size, interpolation=cv2.INTER_AREA)
+    feature_list = []
+    
+    img = getImages(folder_dir)[0]
 
-          #segmentation using region growing, finds the darkest pixel in the image
-          gray_img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-          min_index = np.unravel_index(np.argmin(gray_img), gray_img.shape)
-        
-          segmentation_mask = regionGrowing(gray_img, min_index, threshold=20) #threshold set to lower, maybe do edge dialation
-          
-          #mask for RGB image for feature_C
-          segmentation_mask_rgb = regionGrowing(img, min_index, threshold=20) 
+    img_id = getImages(folder_dir)[1] 
 
-          #Feature extraction
-          A_score = round(mean_asymmetry(segmentation_mask),3)
-          B_score = round(compactness(segmentation_mask),3)
-          red_var, green_var, blue_var = rgb_var(img, slic_segmentation(segmentation_mask_rgb, img))
+    #segmentation using region growing, finds the darkest pixel in the image
+    gray_img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    min_index = np.unravel_index(np.argmin(gray_img), gray_img.shape)
 
-          feature_list.append({
-                'img_id': img_id,  
-                'asymmetry_score': A_score,
-                'border_score': B_score,
-                'color_score_red': round(red_var, 3),
-                'color_score_green': round(green_var, 3),
-                'color_score_blue': round(blue_var, 3),         
+    segmentation_mask = regionGrowing(gray_img, min_index, threshold=20) #threshold set to lower, maybe do edge dialation
+
+    #mask for RGB image for feature_C
+    segmentation_mask_rgb = regionGrowing(img, min_index, threshold=20) 
+
+    #Feature extraction
+    A_score = round(mean_asymmetry(segmentation_mask),3)
+    B_score = round(compactness(segmentation_mask),3)
+    red_var, green_var, blue_var = rgb_var(img, slic_segmentation(segmentation_mask_rgb, img))
+
+    feature_list.append({
+        'img_id': img_id,  
+        'asymmetry_score': A_score,
+        'border_score': B_score,
+        'color_score_red': round(red_var, 3),
+        'color_score_green': round(green_var, 3),
+        'color_score_blue': round(blue_var, 3),         
             })
-  df = pd.DataFrame(feature_list)
-  return df
+    df = pd.DataFrame(feature_list)
+    return df
 
 
 df = extract_features(r"data")
--- util/img_util.py
+++ util/img_util.py
@@ -1,59 +1,81 @@
 import random
-
+import os
+from os import listdir
+import numpy as np
+import cv2
+import pandas as pd
 import cv2
 
 
-def readImageFile(file_path):
-    # read image as an 8-bit array
-    img_bgr = cv2.imread(file_path)
+# def readImageFile(file_path):
+#     # read image as an 8-bit array
+#     img_bgr = cv2.imread(file_path)
 
-    # convert to RGB
-    img_rgb = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)
+#     # convert to RGB
+#     img_rgb = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)
 
-    # convert the original image to grayscale
-    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2GRAY)
+#     # convert the original image to grayscale
+#     img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2GRAY)
 
-    return img_rgb, img_gray
-
-
-def saveImageFile(img_rgb, file_path):
-    try:
-        # convert BGR
-        img_bgr = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2BGR)
-
-        # save the image
-        success = cv2.imwrite(file_path, img_bgr)
-        if not success:
-            print(f"Failed to save the image to {file_path}")
-        return success
-
-    except Exception as e:
-        print(f"Error saving the image: {e}")
-        return False
+#     return img_rgb, img_gray
 
 
-class ImageDataLoader:
-    def __init__(self, directory, shuffle=False, transform=None):
-        self.directory = directory
-        self.shuffle = shuffle
-        self.transform = transform
+# def saveImageFile(img_rgb, file_path):
+#     try:
+#         # convert BGR
+#         img_bgr = cv2.cvtColor(img_rgb, cv2.COLOR_RGB2BGR)
 
-        # get a sorted list of all files in the directory
-        # fill in with your own code below
+#         # save the image
+#         success = cv2.imwrite(file_path, img_bgr)
+#         if not success:
+#             print(f"Failed to save the image to {file_path}")
+#         return success
 
-        if not self.file_list:
-            raise ValueError("No image files found in the directory.")
+#     except Exception as e:
+#         print(f"Error saving the image: {e}")
+#         return False
 
-        # shuffle file list if required
-        if self.shuffle:
-            random.shuffle(self.file_list)
 
-        # get the total number of batches
-        self.num_batches = len(self.file_list)
+# class ImageDataLoader:
+#     def __init__(self, directory, shuffle=False, transform=None):
+#         self.directory = directory
+#         self.shuffle = shuffle
+#         self.transform = transform
 
-    def __len__(self):
-        return self.num_batches
+#         # get a sorted list of all files in the directory
+#         # fill in with your own code below
 
-    def __iter__(self):
-        # fill in with your own code below
-        pass
+#         if not self.file_list:
+#             raise ValueError("No image files found in the directory.")
+
+#         # shuffle file list if required
+#         if self.shuffle:
+#             random.shuffle(self.file_list)
+
+#         # get the total number of batches
+#         self.num_batches = len(self.file_list)
+
+#     def __len__(self):
+#         return self.num_batches
+
+#     def __iter__(self):
+#         # fill in with your own code below
+#         pass
+
+
+def getImages(folder_dir):
+    for filename in os.listdir(folder_dir):
+      if filename.endswith(".png"):
+          img_id = filename 
+          file_path = os.path.join(folder_dir, filename)
+          img = cv2.imread(file_path)
+          
+          #resize if needed
+          max_size = 256
+          h, w = img.shape[:2]
+          scale = min(max_size / w, max_size / h)
+          if scale < 1:
+              new_size = (int(w * scale), int(h * scale))
+              img = cv2.resize(img, new_size, interpolation=cv2.INTER_AREA)
+
+    return img, img_id  
