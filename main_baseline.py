e263a1fcb6010e44cc93436f707afeee50427ebe
Sara <saracetin889@gmail.com>
2025-05-07 11:08:05 +0200

    updated main

--- main_baseline.py
+++ main_baseline.py
@@ -12,40 +12,45 @@ from util.feature_C import slic_segmentation
 from util.classifier_LogReg import logistic_regression_classifier
 from util.img_util import getImages
 
+from joblib import Parallel, delayed
 
-def extract_features(folder_dir):
-    feature_list = []
+
+def extract_feature_baseline(img, img_id):
     
-    images, img_ids = getImages(folder_dir)  
+    #segmentation using region growing, finds the darkest pixel in the image
+    gray_img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    min_index = np.unravel_index(np.argmin(gray_img), gray_img.shape)
 
-    for img, img_id in zip(images, img_ids):
-        #segmentation using region growing, finds the darkest pixel in the image
-        gray_img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-        min_index = np.unravel_index(np.argmin(gray_img), gray_img.shape)
+    segmentation_mask = regionGrowing(gray_img, min_index, threshold=20) #threshold set to lower, maybe do edge dialation
 
-        segmentation_mask = regionGrowing(gray_img, min_index, threshold=20) #threshold set to lower, maybe do edge dialation
+    #Feature extraction
+    A_score = round(mean_asymmetry(segmentation_mask),3)
+    B_score = round(compactness(segmentation_mask),3)
+    red_var, green_var, blue_var = rgb_var(img, slic_segmentation(segmentation_mask, img))
 
-        #Feature extraction
-        A_score = round(mean_asymmetry(segmentation_mask),3)
-        B_score = round(compactness(segmentation_mask),3)
-        red_var, green_var, blue_var = rgb_var(img, slic_segmentation(segmentation_mask, img))
+    return {
+        'img_id': img_id,
+        'asymmetry_score': A_score,
+        'border_score': B_score,
+        'color_score_red': round(red_var, 3),
+        'color_score_green': round(green_var, 3),
+        'color_score_blue': round(blue_var, 3)
+    }
 
-        feature_list.append({
-            'img_id': img_id,  
-            'asymmetry_score': A_score,
-            'border_score': B_score,
-            'color_score_red': round(red_var, 3),
-            'color_score_green': round(green_var, 3),
-            'color_score_blue': round(blue_var, 3),         
-            })
-    df = pd.DataFrame(feature_list)
-    return df
+def extract_df_baseline(folder_dir, n_jobs=1):
+    images, img_ids = getImages(folder_dir)
+    
+    # Process images in parallel if n_jobs > 1
+    features = Parallel(n_jobs=n_jobs)(
+        delayed(extract_feature_baseline)(img, img_id) for img, img_id in zip(images, img_ids)
+    )
+    
+    return pd.DataFrame(features)
 
+#df = extract_df_baseline(r"data")
+#labels_df = pd.read_csv(r"dataset.csv")  
 
-# df = extract_features(r"data")
-# labels_df = pd.read_csv(r"dataset.csv")  
-
-# df_with_labels = df.merge(labels_df, on="img_id", how="inner")
-# result = logistic_regression_classifier(df_with_labels, use_smote=False) # for testing purposes
+#df_with_labels = df.merge(labels_df, on="img_id", how="inner")
+#result = logistic_regression_classifier(df_with_labels, use_smote=False) # for testing purposes
 
 #print(result[2])
--- main_extended.py
+++ main_extended.py
@@ -14,45 +14,50 @@ from util.img_util import getImages
 from util.blueveil import measure_blue_veil
 from util.inpaint_util import removeHair
 
+from joblib import Parallel, delayed
 
-def extract_features(folder_dir):
-    feature_list = []
-    
-    images, img_ids = getImages(folder_dir)  
 
-    for img, img_id in zip(images, img_ids):
-        #segmentation using region growing, finds the darkest pixel in the image
-        gray_img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-        min_index = np.unravel_index(np.argmin(gray_img), gray_img.shape)
+def extract_feature_extended(img, img_id):
 
-        #hair removail output to work further with
-        img_inpaint = removeHair(img, gray_img)
+    #segmentation using region growing, finds the darkest pixel in the image
+    gray_img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    min_index = np.unravel_index(np.argmin(gray_img), gray_img.shape)
+
+    #hair removail output to work further with
+    img_inpaint = removeHair(img, gray_img)
         
-        segmentation_mask = regionGrowing(gray_img, min_index, threshold=20)
+    segmentation_mask = regionGrowing(gray_img, min_index, threshold=20)
 
-        #Feature extraction
-        A_score = round(mean_asymmetry(segmentation_mask),3)
-        B_score = round(compactness(segmentation_mask),3)
-        red_var, green_var, blue_var = rgb_var(img_inpaint, slic_segmentation(segmentation_mask, img_inpaint))
-        blue_veil = round(measure_blue_veil(img_inpaint),3)
+    #Feature extraction
+    A_score = round(mean_asymmetry(segmentation_mask),3)
+    B_score = round(compactness(segmentation_mask),3)
+    red_var, green_var, blue_var = rgb_var(img_inpaint, slic_segmentation(segmentation_mask, img_inpaint))
+    blue_veil = round(measure_blue_veil(img_inpaint),3)
 
-        feature_list.append({
-            'img_id': img_id,  
-            'asymmetry_score': A_score,
-            'border_score': B_score,
-            'color_score_red': round(red_var, 3),
-            'color_score_green': round(green_var, 3),
-            'color_score_blue': round(blue_var, 3), 
-            'blue_veil_score' : blue_veil        
-            })
-    df = pd.DataFrame(feature_list)
-    return df
+    return {
+        'img_id': img_id,  
+        'asymmetry_score': A_score,
+        'border_score': B_score,
+        'color_score_red': round(red_var, 3),
+        'color_score_green': round(green_var, 3),
+        'color_score_blue': round(blue_var, 3), 
+        'blue_veil_score' : blue_veil        
+            }
+
+def extract_df_extended(folder_dir, n_jobs=1):
+    images, img_ids = getImages(folder_dir)
+    
+    # Process images in parallel if n_jobs > 1
+    features = Parallel(n_jobs=n_jobs)(
+        delayed(extract_feature_extended)(img, img_id) for img, img_id in zip(images, img_ids)
+    )
+    return pd.DataFrame(features)
 
 
-# df = extract_features(r"data")
-# labels_df = pd.read_csv(r"dataset.csv")  
+#df = extract_df_extended(r"data")
+#labels_df = pd.read_csv(r"dataset.csv")  
 
-# df_with_labels = df.merge(labels_df, on="img_id", how="inner")
-# result = logistic_regression_classifier(df_with_labels, use_smote=False) # for testing purposes
+#df_with_labels = df.merge(labels_df, on="img_id", how="inner")
+#result = random_forest_classifier(df_with_labels, use_smote=False) 
 
-#print(result[2])
+#print(result[something])
